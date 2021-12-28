b25debe4ef03943bf8a942f45ca8905d66ab0b1b
Beergrizl <a.sadovskii@tut.by>
2021-12-28 20:37:00 +0300

    add redux, react devtools

--- src/Redux/redux-store.tsx
+++ src/Redux/redux-store.tsx
@@ -1,8 +1,8 @@
-import {applyMiddleware, combineReducers, createStore, Store} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore, Store} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogReducer} from "./dialog-reducer";
 import {usersReducer} from "./users-reducer";
-import { authReducer, setAuthUserData} from "./auth-reducer";
+import {authReducer, setAuthUserData} from "./auth-reducer";
 import thunkMiddleware, {ThunkAction} from 'redux-thunk';
 import {reducer as formReducer, stopSubmit} from 'redux-form';
 import {appReducer, initializedSuccess} from "./app-reducer";
@@ -17,18 +17,18 @@ export let reducer = combineReducers({
 })
 
 
-
-
-
-
-
 export type RootReduxStoreType = ReturnType<typeof reducer>;
-export let store: Store = createStore(reducer, applyMiddleware(thunkMiddleware));
 
- type AppActionTypes = ReturnType<typeof setAuthUserData>
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store: Store = createStore(reducer, composeEnhancers(applyMiddleware(thunkMiddleware)
+));
+//export let store: Store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+type AppActionTypes = ReturnType<typeof setAuthUserData>
     | ReturnType<typeof stopSubmit>
     | ReturnType<typeof initializedSuccess>
 
-    export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, RootReduxStoreType, unknown, AppActionTypes>
+export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, RootReduxStoreType, unknown, AppActionTypes>
 
 
